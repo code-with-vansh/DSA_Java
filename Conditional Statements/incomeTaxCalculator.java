44d3bf98cc88e7b979159ba03630d1da7bd46c81
Vansh Chauhan <vanshchauhan.tech@gmail.com>
2026-09-16 03:12:26 +0530

    Modify income input and tax calculation logic
    Updated income input to be in lakhs per annum and adjusted tax calculations accordingly.

--- Conditional Statements/incomeTaxCalculator.java
+++ Conditional Statements/incomeTaxCalculator.java
@@ -6,21 +6,21 @@ public class incomeTaxCalculator {
 
     public static void main (String arg[]) {
         System.out.println("Income Tax Calculator");
-        System.out.print("Enter your income : ");
-        int income = sc.nextInt();
+        System.out.print("Enter your income (lakh per annum) : ");
+        float income = sc.nextFloat();
         int tax;
 
-        if (income < 500000) {
-            tax = 0;
+        if (income < 5) {
+            System.out.print("Income tax (0%) = 0");
         }
-        else if (income >= 500000 && income <1000000) {
-            tax = income * 2 / 100;
+        else if (income >= 5 && income <10) {
+            tax = (int) (income * 20000);
+            System.out.printf("Income tax (20%%) = %d", tax);
         }
         else {
-            tax = income * 3 / 100;
+            tax = (int) (income * 30000);
+            System.out.printf("Income tax (30%%) = %d", tax);
         }
-
-        System.out.printf("\nThe Tax on your %d income is %d", income, tax);
 
     }
 }
