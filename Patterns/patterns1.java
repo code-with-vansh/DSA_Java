2e60b8856390c71441b49eca82a8704e83acaf8f
Vansh Chauhan <vanshchauhan.tech@gmail.com>
2026-09-18 01:38:03 +0530

    Half Pyramid Number Pattern Added

--- Patterns/patterns1.java
+++ Patterns/patterns1.java
@@ -15,8 +15,17 @@ public class patterns1 {
 
     public static void inverted_Half_Pyramid_Star_Pattern(int n) {
         for (int i = 1; i <= n; i++) {
-            for (int j = n-i+1; j >= 1; j--) {
+            for (int j = n - i + 1; j >= 1; j--) {
                 System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void half_Pyramid_Number_Pattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.printf("%d ", j);
             }
             System.out.println();
         }
@@ -29,6 +38,7 @@ public class patterns1 {
             System.out.println("WELCOME IN PATTERNS WORLD");
             System.out.println("Enter 1 : Half Pyramid Star Pattern");
             System.out.println("Enter 2 : Inverted Half Pyramid Star Pattern");
+            System.out.println("Enter 3 : Half Pyramid Number Pattern");
             System.out.println("Enter 0 : EXIT");
 
             System.out.print("Enter your choise : ");
@@ -43,6 +53,9 @@ public class patterns1 {
                     break;
                 case 2:
                     inverted_Half_Pyramid_Star_Pattern(n);
+                    break;
+                case 3:
+                    half_Pyramid_Number_Pattern(n);
                     break;
                 case 0:
                     System.exit(0);
