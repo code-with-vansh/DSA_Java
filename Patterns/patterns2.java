5c9bb9566a6b8f4bc27200dcc5c80f32a6ee6612
Vansh Chauhan <vanshchauhan.tech@gmail.com>
2026-09-19 02:14:31 +0530

    0-1 Triangle Pattern Added

--- Patterns/patterns2.java
+++ Patterns/patterns2.java
@@ -15,12 +15,26 @@ public class patterns2 {
         }
     }
 
+    public static void triangle_0_1_pattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
 
         while (true) {
 
             System.out.println("WELCOME IN PATTERNS WORLD");
             System.out.println("Enter 1 : Floyds Triangle Pattern");
+            System.out.println("Enter 2 : 0-1 Triangle Pattern");
             System.out.println("Enter 0 : EXIT");
 
             System.out.print("Enter your choise : ");
@@ -32,6 +46,9 @@ public class patterns2 {
             switch (choise) {
                 case 1:
                     floyds_triangle_pattern(n);
+                    break;
+                case 2:
+                    triangle_0_1_pattern(n);
                     break;
                 case 0:
                     System.exit(0);
